8cf06a19eaad35285be69293ac7702807c5c2855
n4tn <109428583+n4tn@users.noreply.github.com>
2022-11-01 14:58:35 +0000

    Starting on Moq- waiting on services

--- TraineeTracker/TraineeTrackerTests/ControllerTests.cs
+++ TraineeTracker/TraineeTrackerTests/ControllerTests.cs
@@ -1,17 +1,26 @@
+using Moq;
+using TraineeTracker.Controllers;
+using TraineeTracker.Data;
+using TraineeTracker.Models;
+using TraineeTracker.Service;
+
 namespace TraineeTrackerTests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
+    //    [SetUp]
+    //    public void Setup()
+    //    {
 
-        }
+    //    }
+    private UserDatasController? _sut;
 
         [Test]
-        public void Test1()
+        public void UserDatasController_CanBe_Constructed()
         {
-            Assert.Pass();
+        var mockService = new Mock<TraineeTrackerContext>(null);
+        _sut = new UserDatasController(mockService.Object);
+        Assert.That(_sut, Is.InstanceOf<UserDatasController>());
         }
     }
 }
