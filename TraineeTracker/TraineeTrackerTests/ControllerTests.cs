7441009ad4567c75ab2805e1a15ff17d407be655
n4tn <109428583+n4tn@users.noreply.github.com>
2022-11-02 17:29:51 +0000

    Moq 51% Test coverage

--- TraineeTracker/TraineeTrackerTests/ControllerTests.cs
+++ TraineeTracker/TraineeTrackerTests/ControllerTests.cs
@@ -13,66 +13,80 @@ namespace TraineeTrackerTests
     {
         private UserDatasController? _sut;
 
-        [SetUp]
-        public void SetUp()
-        {
-            var mockService = new Mock<IServiceLayer<UserData>>();
-            var mockUser = new Mock<IUserManager<User>>();
-            var mockTracker = new Mock<IServiceLayer<Tracker>>();
-            _sut = new UserDatasController(mockService.Object, mockUser.Object, mockTracker.Object);
-        }
-
         [Test]
         [Category("Constructor")]
         public void GivenDataAndUser_WhenConstructorIsCalled_ConstructorIsConstructed()
         {
-            Assert.That(_sut, Is.InstanceOf<UserDatasController>());
-        }
-        [Ignore("return view")]
-        [Test]
-        [Category("Trainee Path")]
-        public void GivenTrainee_WhenIndexIsCalledAsTrainee_ReturnsViewForTrainee()
-        {
             var mockService = new Mock<IServiceLayer<UserData>>();
             var mockUser = new Mock<IUserManager<User>>();
             var mockTracker = new Mock<IServiceLayer<Tracker>>();
             _sut = new UserDatasController(mockService.Object, mockUser.Object, mockTracker.Object);
-
-            var user = mockUser.Setup(x => x.GetUserAsync()).Returns(Task.FromResult(new User()));
-            
-            var result = _sut.Index().Result;
-
-            Assert.That(result, Is.EqualTo(user));
+            Assert.That(_sut, Is.InstanceOf<UserDatasController>());
         }
         [Test]
         [Category("Trainer Path")]
         public void GivenTrainer_WhenIndexIsCalledAsTrainer_ItIsNotNull()
         {
+            var mockService = new Mock<IServiceLayer<UserData>>();
             var mockUser = new Mock<IUserManager<User>>();
+            var mockTracker = new Mock<IServiceLayer<Tracker>>();
 
-            var mockTrainer = mockUser.Setup(x => x.IsInRole("Trainer")).Returns(true);
+            mockUser.Setup(x => x.IsInRole("Trainer")).Returns(true);
+            _sut = new UserDatasController(mockService.Object, mockUser.Object, mockTracker.Object);
+
+            var result = _sut.Index().Result;
+
+            Assert.That(result, Is.Not.False);
+            Assert.That(result, Is.Not.Null);
             
-            Assert.That(mockTrainer, Is.Not.False);
-            Assert.That(mockTrainer, Is.Not.Null);
-            
+        }
+        [Test]
+        [Category("Trainer Path")]
+        public void GivenTrainee_WhenIndexIsCalledAsTrainee_ItIsNotNull()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            var mockTracker = new Mock<IServiceLayer<Tracker>>();
+
+            mockUser.Setup(x => x.IsInRole("Trainee")).Returns(true);
+            _sut = new UserDatasController(mockService.Object, mockUser.Object, mockTracker.Object);
+
+            var result = _sut.Index().Result;
+
+            Assert.That(result, Is.Not.False);
+            Assert.That(result, Is.Not.Null);
+
         }
         [Test]
         [Category("Index - ServiceGetAllAsync - Happy Path")]
         public void GivenUserDataInDatabase_WhenGetAllAsyncIsCalled_ReturnsListOfUserData()
         {
             var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            var mockTracker = new Mock<IServiceLayer<Tracker>>();
 
-            var mockUserData = mockService.Setup(x => x.GetAllAsync().Result).Returns(It.IsAny<IEnumerable<UserData>>());
+            mockService.Setup(x => x.GetAllAsync().Result);
+            _sut = new UserDatasController(mockService.Object, mockUser.Object, mockTracker.Object);
 
-            Assert.That(mockUserData, Is.Not.Null);
+            var result = _sut.Index().Result;
+
+            Assert.That(result, Is.Not.Null);
         }
         [Test]
         [Category("Details - ServiceFindAsync - Happy Path")]
         public void GivenValidID_WhenServiceFindAsyncIsCalled_ItIsNotNull()
         {
             var mockService = new Mock<IServiceLayer<UserData>>();
-            var mockFindAsync = mockService.Setup(x => x.FindAsync(It.IsAny<int>()).Result);
-            Assert.That(mockFindAsync, Is.Not.Null);
+            var mockUser = new Mock<IUserManager<User>>();
+            var mockTracker = new Mock<IServiceLayer<Tracker>>();
+
+            mockService.Setup(x => x.FindAsync(It.IsAny<int>()).Result);
+            _sut = new UserDatasController(mockService.Object, mockUser.Object, mockTracker.Object);
+
+            var result = _sut.Details(1).Result;
+
+            Assert.That(result, Is.Not.Null);
+            
         }
         [Ignore("need to return null")]
         [Test]
@@ -85,62 +99,94 @@ namespace TraineeTrackerTests
         [Category("TrackerDetails - TrackerFindAsync - Happy Path")]
         public void GivenValidID_TrackerFindAsync_IsNotNull()
         {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
             var mockTracker = new Mock<IServiceLayer<Tracker>>();
 
-            var mockFindAsync = mockTracker.Setup(x => x.FindAsync(It.IsAny<int>()).Result);
+            mockTracker.Setup(x => x.FindAsync(It.IsAny<int>()).Result);
+            _sut = new UserDatasController(mockService.Object, mockUser.Object, mockTracker.Object);
+
+            var result = _sut.TrackerDetails(1).Result;
 
             //Assert.That(FindAsync, Is.TypeOf<ViewResult>());
-            Assert.That(mockFindAsync, Is.Not.Null);
+            Assert.That(result, Is.Not.Null);
         }
         [Test]
         [Category("Create GET")]
         public void GivenUserData_WhenCreateConstructorIsCalled_RetunsView()
         {
-
-            var mockCreate = _sut.Create();
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            var mockTracker = new Mock<IServiceLayer<Tracker>>();
             
-            Assert.That(mockCreate, Is.TypeOf<ViewResult>());
+            _sut = new UserDatasController(mockService.Object, mockUser.Object, mockTracker.Object);
+            
+            var result = _sut.Create();
+            
+            Assert.That(result, Is.TypeOf<ViewResult>());
         }
         [Test]
         [Category("Create POST")]
         public void GivenUserData_WhenAddAsyncIsCalled_ItIsNotNull()
         {
             var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            var mockTracker = new Mock<IServiceLayer<Tracker>>();
 
-            var mockAddAsync = mockService.Setup( x => x.AddAsync(It.IsAny<UserData>()));
+            mockService.Setup( x => x.AddAsync(It.IsAny<UserData>()));
+            _sut = new UserDatasController(mockService.Object, mockUser.Object, mockTracker.Object);
 
-            Assert.That(mockAddAsync, Is.Not.Null);
+            var data = new UserData { FirstName = It.IsAny<string>(), LastName = It.IsAny<string>(), Title = It.IsAny<string>(), Education = It.IsAny<string>(), Experience = It.IsAny<string>(), Activity = It.IsAny<string>(), Biography = It.IsAny<string>(), Skills = It.IsAny<string>(), ID = 1, UserID = It.IsAny<string>(), Roles = UserData.Level.Trainee };
+            var result = _sut.Create(data).Result;
+            
+            Assert.That(result, Is.Not.Null);
         }
         [Test]
         [Category("Edit POST - Update")]
         public void GivenValidUserData_WhenUpdateIsCalled_ItIsNotNull()
         {
             var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            var mockTracker = new Mock<IServiceLayer<Tracker>>();
 
-            var mockUpdate = mockService.Setup(x => x.Update(It.IsAny<UserData>()));
+            mockService.Setup(x => x.Update(It.IsAny<UserData>()));
+            _sut = new UserDatasController(mockService.Object, mockUser.Object, mockTracker.Object);
 
-            Assert.That(mockUpdate, Is.Not.Null);
+            var data = new UserData { FirstName = It.IsAny<string>(), LastName = It.IsAny<string>(), Title = It.IsAny<string>(), Education = It.IsAny<string>(), Experience = It.IsAny<string>(), Activity = It.IsAny<string>(), Biography = It.IsAny<string>(), Skills = It.IsAny<string>(), ID = 1, UserID = It.IsAny<string>(), Roles = UserData.Level.Trainee };
+            var result = _sut.Edit(1, data).Result;
+            
+            Assert.That(result, Is.Not.Null);
+
+            //mockService.Verify(x => x.Update(It.IsAny<UserData>()), Times.Once);
+            
         }
         [Test]
         [Category("Edit Post - Exists - Happy Path")]
         public void GivenValidUserID_WhenExistsIsCalled_ItIsNotNull()
         {
             var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            var mockTracker = new Mock<IServiceLayer<Tracker>>();
+                        
+            mockService.Setup(x => x.Exists(It.IsAny<int>()));
+            _sut = new UserDatasController(mockService.Object, mockUser.Object, mockTracker.Object);
 
-            var mockExists = mockService.Setup(x => x.Exists(It.IsAny<int>())).Returns(true);
-
-            Assert.That(mockExists, Is.Not.Null);
+            var data = new UserData { FirstName = It.IsAny<string>(), LastName = It.IsAny<string>(), Title = It.IsAny<string>(), Education = It.IsAny<string>(), Experience = It.IsAny<string>(), Activity = It.IsAny<string>(), Biography = It.IsAny<string>(), Skills = It.IsAny<string>(), ID = 1, UserID = It.IsAny<string>(), Roles = UserData.Level.Trainee };
+            var result = _sut.Edit(1, data).Result;
+            
+            Assert.That(result, Is.Not.Null);
             
 
+
         }
-        [Ignore("need to return the result")]
+        [Ignore("WIP")]
         [Test]
         [Category("Edit Post - Exists - Sad Path")]
         public void x()
         {
-            var mockService = new Mock<IServiceLayer<UserData>>();
+            //var mockService = new Mock<IServiceLayer<UserData>>();
 
-            var mockExists = mockService.Setup(x => x.Exists(It.IsAny<int>())).Returns(false);
+            //var mockExists = mockService.Setup(x => x.Exists(It.IsAny<int>())).Returns(false);
 
            
             //mockExists.Verifiable(StatusCodes.Status404NotFound);
