354e5a1f11b99b4a96ba76cfe62fab06360968c3
SahilSingh021 <sahilsingh.preet21@gmail.com>
2022-11-02 17:07:36 +0000

    UI Improvements

--- TraineeTracker/TraineeTracker/Models/ViewModels/TrackerViewModel.cs
+++ TraineeTracker/TraineeTracker/Models/ViewModels/TrackerViewModel.cs
@@ -2,11 +2,11 @@
 {
     public class TrackerViewModel
     {
-        public int UserDataId { get; set; }
-        public int Week { get; set; }
-        public string Stop { get; set; } = "";
-        public string Start { get; set; } = "";
-        public string Continue { get; set; } = "";
+        public int UserDataId { get; init; }
+        public int Week { get; init; }
+        public string Stop { get; set; } = "N/A";
+        public string Start { get; set; } = "N/A";
+        public string Continue { get; set; } = "N/A";
         public string Comments { get; set; } = "";
         public int TechnicalSkills { get; set; } = 0;
         public int ConsultantSkills { get; set; } = 0;
