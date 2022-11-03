48d9e9c70c88e9151605c346281871323aac20c2
Lewis Kellett <lewisk1996@gmail.com>
2022-11-03 08:26:17 +0000

    Refactored Controller tests, Added TrackerCcontroller Tests, Refactored UserDatasController

---
+++ TraineeTracker/TraineeTrackerTests/TrackersController_Tests.cs
@@ -0,0 +1,331 @@
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Moq;
+using System.Xml.Linq;
+using TraineeTracker.Controllers;
+using TraineeTracker.Data;
+using TraineeTracker.Models;
+using TraineeTracker.Models.ViewModels;
+using TraineeTracker.Service;
+
+
+namespace TraineeTrackerTests
+{
+    public class TrackersController_Tests
+    {
+        private TrackersController? _sut;
+
+        [Test]
+        [Category("Constructor")]
+        [Category("Happy Path")]
+        public void WhenConstructorIsCalled_ObjectIsConstructed()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+            
+            _sut = new TrackersController(mockService.Object);
+            Assert.That(_sut, Is.Not.Null);
+            Assert.That(_sut, Is.InstanceOf<TrackersController>());
+        }
+
+        [Test]
+        [Category("Trainer Path")]
+        [Category("Happy Path")]
+        public void WhenIndexIsCalledAs_Trainer_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+
+            mockService.Setup(x => x.GetAllAsync())
+                .Returns(Task.FromResult(((IEnumerable<Tracker>)new List<Tracker>() { new Tracker() { Comments = "ABCD" } })));
+
+            _sut = new TrackersController(mockService.Object);
+
+            var result = ((ViewResult)_sut.Index(It.IsAny<int>()).Result).Model;
+            List<TrackerViewModel> list = (List<TrackerViewModel>)result!;
+
+            mockService.Verify(x => x.GetAllAsync(), Times.Once);
+            Assert.That(result, Is.Not.Null);
+            Assert.That(list.Count, Is.EqualTo(1));
+            Assert.That(list[0]!.Comments, Is.EqualTo("ABCD"));
+        }
+
+        [Test]
+        [Category("Trainee Path")]
+        [Category("Happy Path")]
+        public void WhenIndexIsCalledAs_Trainee_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+            
+            mockService.Setup(x => x.GetAllAsync())
+                .Returns(Task.FromResult(((IEnumerable<Tracker>)new List<Tracker>() { new Tracker() { Comments = "ABCD" } })));
+
+            _sut = new TrackersController(mockService.Object);
+
+            var result = ((ViewResult)_sut.Index(It.IsAny<int>()).Result).Model;
+            List<TrackerViewModel> list = (List<TrackerViewModel>)result!;
+
+            mockService.Verify(x => x.GetAllAsync(), Times.Once);
+            Assert.That(result, Is.Not.Null);
+            Assert.That(list.Count, Is.EqualTo(1));
+            Assert.That(list[0]!.Comments, Is.EqualTo("ABCD"));
+        }
+
+        [Test]
+        [Category("Details Path")]
+        [Category("Sad Path")]
+        public void WhenDetailsIsCalledWith_InvalidId_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+            
+            //mockService.Setup(x => x.FindAsync(It.IsAny<int>()));
+            _sut = new TrackersController(mockService.Object);
+
+            var result = ((NotFoundResult)_sut.Details(null).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Details Path")]
+        [Category("Sad Path")]
+        public void WhenDetailsIsCalledWith_NullService_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+            
+            mockService.Setup(x => x.IsNull()).Returns(true);
+            _sut = new TrackersController(mockService.Object);
+
+            var result = ((NotFoundResult)_sut.Details(null).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Details Path")]
+        [Category("Sad Path")]
+        public void WhenDetailsIsCalledWith_NoTracker_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+            
+            mockService.Setup(x => x.FindAsync(It.IsAny<int>())).Returns(Task.FromResult((Tracker)null!)!);
+            _sut = new TrackersController(mockService.Object);
+
+            var result = ((NotFoundResult)_sut.Details(1).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Details Path")]
+        [Category("Sad Path")]
+        public void WhenDetailsIsCalledWith_ValidData_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+            
+            mockService.Setup(x => x.FindAsync(It.IsAny<int>())).Returns(Task.FromResult(new Tracker() { Comments="TEST"})!);
+            _sut = new TrackersController(mockService.Object);
+
+            var result = (TrackerViewModel)((ViewResult)_sut.Details(1).Result).Model!;
+
+            mockService.Verify(x => x.FindAsync(It.IsAny<int>()), Times.Exactly(1));
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Comments, Is.EqualTo("TEST"));
+        }
+
+        [Test]
+        [Category("Create Path")]
+        [Category("Happy Path")]
+        public void WhenCreateIsCalled_ReturnsExpected_View()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+            _sut = new TrackersController(mockService.Object);
+            var result = _sut.Create();
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Details Path")]
+        [Category("Happy Path")]
+        public void WhenCreateIsCalled_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+
+            mockService.Setup(x => x.AddAsync(It.IsAny<Tracker>()));
+            _sut = new TrackersController(mockService.Object);
+
+            var result = _sut.Create(new Tracker());
+
+            mockService.Verify(x => x.AddAsync(It.IsAny<Tracker>()), Times.Exactly(1));
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Details Path")]
+        [Category("Sad Path")]
+        public void WhenCreateIsCalled_WithInvalidModel_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+
+            mockService.Setup(x => x.AddAsync(It.IsAny<Tracker>()));
+            _sut = new TrackersController(mockService.Object);
+            _sut.ModelState.AddModelError("key", "error message");
+
+            var result = _sut.Create(new Tracker());
+
+            mockService.Verify(x => x.AddAsync(It.IsAny<Tracker>()), Times.Never());
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Edit Path")]
+        [Category("Sad Path")]
+        public void WhenEditIsCalledWith_InvalidId_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+            
+            //mockService.Setup(x => x.FindAsync(It.IsAny<int>()));
+            _sut = new TrackersController(mockService.Object);
+
+            var result = ((NotFoundResult)_sut.Edit(null).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Edit Path")]
+        [Category("Sad Path")]
+        public void WhenEditIsCalledWith_NullService_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+            
+            mockService.Setup(x => x.IsNull()).Returns(true);
+            _sut = new TrackersController(mockService.Object);
+
+            var result = ((NotFoundResult)_sut.Edit(null).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Edit Path")]
+        [Category("Sad Path")]
+        public void WhenEditIsCalledWith_NoTracker_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+            
+            mockService.Setup(x => x.FindAsync(It.IsAny<int>())).Returns(Task.FromResult((Tracker)null!)!);
+            _sut = new TrackersController(mockService.Object);
+
+            var result = ((NotFoundResult)_sut.Edit(1).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Edit Path")]
+        [Category("Sad Path")]
+        public void WhenEditIsCalledWith_ValidData_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+            
+            mockService.Setup(x => x.FindAsync(It.IsAny<int>())).Returns(Task.FromResult(new Tracker() { Comments = "TEST" })!);
+            _sut = new TrackersController(mockService.Object);
+
+            var result = (TrackerViewModel)((ViewResult)_sut.Edit(1).Result).Model!;
+
+            mockService.Verify(x => x.FindAsync(It.IsAny<int>()), Times.Exactly(1));
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Delete Path")]
+        [Category("Sad Path")]
+        public void WhenDeleteIsCalledWith_InvalidId_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+            
+            //mockService.Setup(x => x.FindAsync(It.IsAny<int>()));
+            _sut = new TrackersController(mockService.Object);
+
+            var result = ((NotFoundResult)_sut.Delete(null).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Delete Path")]
+        [Category("Sad Path")]
+        public void WhenDeleteIsCalledWith_NullService_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+            
+            mockService.Setup(x => x.IsNull()).Returns(true);
+            _sut = new TrackersController(mockService.Object);
+
+            var result = ((NotFoundResult)_sut.Delete(null).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Delete Path")]
+        [Category("Sad Path")]
+        public void WhenDeleteIsCalledWith_NoTracker_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+            
+            mockService.Setup(x => x.FindAsync(It.IsAny<int>())).Returns(Task.FromResult((Tracker)null!)!);
+            _sut = new TrackersController(mockService.Object);
+
+            var result = ((NotFoundResult)_sut.Delete(1).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Delete Path")]
+        [Category("Sad Path")]
+        public void WhenDeleteIsCalledWith_ValidData_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+            
+            mockService.Setup(x => x.FindAsync(It.IsAny<int>())).Returns(Task.FromResult(new Tracker() { Comments = "TEST" })!);
+            _sut = new TrackersController(mockService.Object);
+
+            var result = (TrackerViewModel)((ViewResult)_sut.Delete(1).Result).Model!;
+
+            mockService.Verify(x => x.FindAsync(It.IsAny<int>()), Times.Exactly(1));
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("DeleteConfirmed Path")]
+        [Category("Sad Path")]
+        public void WhenDeleteConfirmedIsCalledWith_NullService_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+            
+            mockService.Setup(x => x.IsNull()).Returns(true);
+            _sut = new TrackersController(mockService.Object);
+
+            var errorString = "Entity set 'TraineeTrackerContext.TrackerDB'  is null.";
+
+            var result = ((ObjectResult)_sut.DeleteConfirmed(1).Result);
+            var expected = new ObjectResult(new ProblemDetails { Detail = errorString });
+
+            Assert.That(result, Is.InstanceOf<ObjectResult>());
+            Assert.That(((ProblemDetails)result.Value!).Detail, Is.EqualTo(errorString));
+        }
+
+        [Test]
+        [Category("DeleteConfirmed Path")]
+        [Category("Happy Path")]
+        public void WhenDeleteConfirmedIsCalledWith_ValidUserId_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<Tracker>>();
+            
+            mockService.Setup(x => x.IsNull()).Returns(false);
+            mockService.Setup(x => x.FindAsync(It.IsAny<int>())).Returns(Task.FromResult(new Tracker() { Comments = "TEST" })!);
+            mockService.Setup(x => x.RemoveAsync(It.IsAny<Tracker>()));
+
+            _sut = new TrackersController(mockService.Object);
+
+            var result = _sut.DeleteConfirmed(1).Result;
+
+            mockService.Verify(x => x.RemoveAsync(It.IsAny<Tracker>()), Times.Once);
+            Assert.That(result, Is.InstanceOf<RedirectToActionResult>());
+        }
+    }
+}
---
+++ TraineeTracker/TraineeTrackerTests/UserDataController_Tests.cs
@@ -0,0 +1,319 @@
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Moq;
+using TraineeTracker.Controllers;
+using TraineeTracker.Data;
+using TraineeTracker.Models;
+using TraineeTracker.Models.ViewModels;
+using TraineeTracker.Service;
+
+
+namespace TraineeTrackerTests
+{
+    public class UserDataController_Tests
+    {
+        private UserDatasController? _sut;
+
+        [Test]
+        [Category("Constructor")]
+        [Category("Happy Path")]
+        public void WhenConstructorIsCalled_ObjectIsConstructed()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+            Assert.That(_sut, Is.Not.Null);
+            Assert.That(_sut, Is.InstanceOf<UserDatasController>());
+        }
+
+        [Test]
+        [Category("Trainer Path")]
+        [Category("Happy Path")]
+        public void WhenIndexIsCalledAs_Trainer_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+
+            mockUser.Setup(x => x.IsInRole("Trainer")).Returns(true);
+            mockUser.Setup(x => x.GetUserAsync()).Returns(Task.FromResult(new User() { Id = "AAA" }));
+            mockService.Setup(x => x.GetAllAsync())
+                .Returns(Task.FromResult(((IEnumerable<UserData>)new List<UserData>() { new UserData() { UserID = "AAA", Activity = "ABCD" } })));
+
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+
+            var result = ((ViewResult)_sut.Index().Result).Model;
+            List<UserDataViewModel> list = (List<UserDataViewModel>)result!;
+
+            mockUser.Verify(x => x.IsInRole("Trainer"), Times.Once);
+            mockUser.Verify(x => x.GetUserAsync(), Times.Once);
+            mockService.Verify(x => x.GetAllAsync(), Times.Once);
+            Assert.That(result, Is.Not.Null);
+            Assert.That(list.Count, Is.EqualTo(1));
+            Assert.That(list[0]!.Activity, Is.EqualTo("ABCD"));
+        }
+
+        [Test]
+        [Category("Trainee Path")]
+        [Category("Happy Path")]
+        public void WhenIndexIsCalledAs_Trainee_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+
+            mockUser.Setup(x => x.IsInRole("Trainee")).Returns(true);
+            mockUser.Setup(x => x.GetUserAsync()).Returns(Task.FromResult(new User() { Id = "AAA" }));
+            mockService.Setup(x => x.GetAllAsync())
+                .Returns(Task.FromResult(((IEnumerable<UserData>)new List<UserData>() { new UserData() { UserID = "AAA", Activity = "ABCD" } })));
+
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+
+            var result = ((ViewResult)_sut.Index().Result).Model;
+            List<UserDataViewModel> list = (List<UserDataViewModel>)result!;
+
+            mockUser.Verify(x => x.IsInRole("Trainee"), Times.Once);
+            mockUser.Verify(x => x.GetUserAsync(), Times.Once);
+            mockService.Verify(x => x.GetAllAsync(), Times.Once);
+            Assert.That(result, Is.Not.Null);
+            Assert.That(list.Count, Is.EqualTo(1));
+            Assert.That(list[0]!.Activity, Is.EqualTo("ABCD"));
+        }
+
+        [Test]
+        [Category("Trainee Path")]
+        [Category("Sad Path")]
+        public void WhenIndexIsCalledAs_NonExistantRole_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            mockUser.Setup(x => x.IsInRole(It.IsAny<string>())).Returns(false);
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+
+            var result = ((NoContentResult)_sut.Index().Result);
+            
+            mockUser.Verify(x => x.IsInRole(It.IsAny<string>()), Times.Exactly(2));
+
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Details Path")]
+        [Category("Sad Path")]
+        public void WhenDetailsIsCalledWith_InvalidId_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            //mockService.Setup(x => x.FindAsync(It.IsAny<int>()));
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+
+            var result = ((NotFoundResult)_sut.Details(null).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Details Path")]
+        [Category("Sad Path")]
+        public void WhenDetailsIsCalledWith_NullService_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            mockService.Setup(x => x.IsNull()).Returns(true);
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+
+            var result = ((NotFoundResult)_sut.Details(null).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Details Path")]
+        [Category("Sad Path")]
+        public void WhenDetailsIsCalledWith_NoUserData_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            mockService.Setup(x => x.FindAsync(It.IsAny<int>())).Returns(Task.FromResult((UserData)null!)!);
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+
+            var result = ((NotFoundResult)_sut.Details(1).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Details Path")]
+        [Category("Sad Path")]
+        public void WhenDetailsIsCalledWith_ValidData_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            mockService.Setup(x => x.FindAsync(It.IsAny<int>())).Returns(Task.FromResult(new UserData() { FirstName="TEST"})!);
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+
+            var result = (UserDataViewModel)((ViewResult)_sut.Details(1).Result).Model!;
+
+            mockService.Verify(x => x.FindAsync(It.IsAny<int>()), Times.Exactly(1));
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.FirstName, Is.EqualTo("TEST"));
+
+        }
+
+
+        [Test]
+        [Category("Edit Path")]
+        [Category("Sad Path")]
+        public void WhenEditIsCalledWith_InvalidId_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            //mockService.Setup(x => x.FindAsync(It.IsAny<int>()));
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+
+            var result = ((NotFoundResult)_sut.Edit(null).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Edit Path")]
+        [Category("Sad Path")]
+        public void WhenEditIsCalledWith_NullService_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            mockService.Setup(x => x.IsNull()).Returns(true);
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+
+            var result = ((NotFoundResult)_sut.Edit(null).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Edit Path")]
+        [Category("Sad Path")]
+        public void WhenEditIsCalledWith_NoUserData_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            mockService.Setup(x => x.FindAsync(It.IsAny<int>())).Returns(Task.FromResult((UserData)null!)!);
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+
+            var result = ((NotFoundResult)_sut.Edit(1).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Edit Path")]
+        [Category("Sad Path")]
+        public void WhenEditIsCalledWith_ValidData_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            mockService.Setup(x => x.FindAsync(It.IsAny<int>())).Returns(Task.FromResult(new UserData() { FirstName = "TEST" })!);
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+
+            var result = (UserDataViewModel)((ViewResult)_sut.Edit(1).Result).Model!;
+
+            mockService.Verify(x => x.FindAsync(It.IsAny<int>()), Times.Exactly(1));
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.FirstName, Is.EqualTo("TEST"));
+
+        }
+
+        [Test]
+        [Category("Delete Path")]
+        [Category("Sad Path")]
+        public void WhenDeleteIsCalledWith_InvalidId_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            //mockService.Setup(x => x.FindAsync(It.IsAny<int>()));
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+
+            var result = ((NotFoundResult)_sut.Delete(null).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Delete Path")]
+        [Category("Sad Path")]
+        public void WhenDeleteIsCalledWith_NullService_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            mockService.Setup(x => x.IsNull()).Returns(true);
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+
+            var result = ((NotFoundResult)_sut.Delete(null).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Delete Path")]
+        [Category("Sad Path")]
+        public void WhenDeleteIsCalledWith_NoUserData_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            mockService.Setup(x => x.FindAsync(It.IsAny<int>())).Returns(Task.FromResult((UserData)null!)!);
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+
+            var result = ((NotFoundResult)_sut.Delete(1).Result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        [Category("Delete Path")]
+        [Category("Sad Path")]
+        public void WhenDeleteIsCalledWith_ValidData_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            mockService.Setup(x => x.FindAsync(It.IsAny<int>())).Returns(Task.FromResult(new UserData() { FirstName = "TEST" })!);
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+
+            var result = (UserDataViewModel)((ViewResult)_sut.Delete(1).Result).Model!;
+
+            mockService.Verify(x => x.FindAsync(It.IsAny<int>()), Times.Exactly(1));
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.FirstName, Is.EqualTo("TEST"));
+
+        }
+
+        [Test]
+        [Category("DeleteConfirmed Path")]
+        [Category("Sad Path")]
+        public void WhenDeleteConfirmedIsCalledWith_NullService_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            mockService.Setup(x => x.IsNull()).Returns(true);
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+
+            var errorString = "Entity set 'TraineeTrackerContext.UserDataDB'  is null.";
+
+            var result = ((ObjectResult)_sut.DeleteConfirmed(1).Result);
+            var expected = new ObjectResult(new ProblemDetails { Detail = errorString });
+
+            Assert.That(result, Is.InstanceOf<ObjectResult>());
+            Assert.That(((ProblemDetails)result.Value!).Detail, Is.EqualTo(errorString));
+        }
+
+        [Test]
+        [Category("DeleteConfirmed Path")]
+        [Category("Happy Path")]
+        public void WhenDeleteConfirmedIsCalledWith_ValidUserId_ReturnsExpected()
+        {
+            var mockService = new Mock<IServiceLayer<UserData>>();
+            var mockUser = new Mock<IUserManager<User>>();
+            mockService.Setup(x => x.IsNull()).Returns(false);
+            mockService.Setup(x => x.FindAsync(It.IsAny<int>())).Returns(Task.FromResult(new UserData() { FirstName = "TEST" })!);
+            mockService.Setup(x => x.RemoveAsync(It.IsAny<UserData>()));
+
+            _sut = new UserDatasController(mockService.Object, mockUser.Object);
+
+            var result = _sut.DeleteConfirmed(1).Result;
+
+            mockService.Verify(x => x.RemoveAsync(It.IsAny<UserData>()), Times.Once);
+            Assert.That(result, Is.InstanceOf<RedirectToActionResult>());
+        }
+    }
+}
