ed14a7fa82b45337aea8a563f4768c1edf0d8199
AdMill21 <admillard01@gmail.com>
2022-12-13 11:06:38 +0000

    app config and test host

---
+++ TraineeTracker/TraineeTrackerTests/lib/AppConfigReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraineeTrackerTests.lib
+{
+    public class AppConfigReader
+    {
+        //public static readonly string BaseUrl = ConfigurationManager.AppSettings["base_url"];
+        //public static readonly string LoginUrl = ConfigurationManager.AppSettings["login_url"];
+    }
+}
---
+++ TraineeTracker/TraineeTrackerTests/lib/pages/SL_LoginPage.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraineeTrackerTests.lib.pages
+{
+    public class SL_LoginPage
+    {
+        private IWebDriver SeleniumDriver { get; }
+        private string _homePageUrl = AppConfigReader.LoginUrl;
+    }
+}
