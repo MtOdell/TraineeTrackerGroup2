f1bcf1ffcafcb96ae11162519963a907566ff6ea
GitAdamReed <57542237+MiniAddman@users.noreply.github.com>
2022-12-13 11:48:17 +0000

    Create POM for Tracker Index page

--- TraineeTracker/TraineeTrackerTests/lib/AppConfigReader.cs
+++ TraineeTracker/TraineeTrackerTests/lib/AppConfigReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,10 @@ namespace TraineeTrackerTests.lib
     {
         //public static readonly string BaseUrl = ConfigurationManager.AppSettings["base_url"];
         //public static readonly string LoginUrl = ConfigurationManager.AppSettings["login_url"];
+        public static readonly string TrackerIndexUrl = ConfigurationManager.AppSettings["tracker_index_url"];
+        public static readonly string TrackerCreateUrl = ConfigurationManager.AppSettings["tracker_create_url"];
+        public static readonly string TrackerDetailsUrl = ConfigurationManager.AppSettings["tracker_details_url"];
+        public static readonly string TrackerDeleteUrl = ConfigurationManager.AppSettings["tracker_delete_url"];
+        public static readonly string TrackerEditUrl = ConfigurationManager.AppSettings["tracker_edit_url"];
     }
 }
