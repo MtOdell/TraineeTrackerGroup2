511aaccff91344b049e1cd2808636058fbb0dfaf
Sergiusz Pietrala <pietralasergiusz@gmail.com>
2022-12-13 10:23:18 +0000

    Added tests folder structure

---
+++ TraineeTracker/TraineeTrackerTests/lib/driver_config/SeleniumDriverConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraineeTrackerTests.lib.driver_config
+{
+    internal class SeleniumDriverConfig
+    {
+    }
+}
